ff3b00a42fa27c6c960b260557816456237d4bda
Dillon de Silva <dillon.desilva@geeksdiary.com>
2018-11-10 16:37:03 +1100

    Successfully implemented basic authentication logic. Minor fixes

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import Admin from 'firebase-admin';
-import serviceAccount from '../serviceAccountKey';
-
+import firebase from 'firebase';
+import { throws } from 'assert';
 class Login extends React.Component {
+  login () {
+    var email = this.refs.email.value;
+    var password = this.refs.password.value;
+
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+      console.log(errorCode, errorMessage);
+    });
+  }
   render () {
     return (
       <div>
         <h1>Login to App</h1>
+        <input type='email' ref='email'/>
+        <input type='password' ref='password'/>
+        <button onClick={() => this.login}>Let's Go</button>
       </div>
     )
   }
